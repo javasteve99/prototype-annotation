1316ec0c17c9cefa30fb2e4a3b04c664498eb817
Steve Hickey <steve@stevehickeydesign.com>
2013-10-11 08:20:00 -0400

    Clean up toggle and transition on annotations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-$('.trigger').on('click', function() {
-  $(this).parent().toggleClass('visible');
-  return false;
-});
+(function () {
+  "use strict";
+  /*jshint validthis:true, jquery:true */
+
+  /* Show annotation on click ============================================== */
+  $('.trigger').on('click', function() {
+    $(this).parent().toggleClass('visible');
+    return false;
+  });
+}());
