239f6f997af835969e2229965b952f55961dd627
Steve Hickey <steve@stevehickeydesign.com>
2013-10-11 10:42:04 -0400

    Get annotation append working

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@
   "use strict";
   /*jshint validthis:true, jquery:true */
 
+  /* Remove elements from end of file and place in proper parents ========== */
+  $('#annotations .annotation').each(function() {
+    var annotationNumber = $(this).attr('id').split('-')[1];
+    $('[data-annotation="' + annotationNumber + '"]').css('position', 'relative').append($('#annotation-' + annotationNumber));
+  });
+
   /* Show annotation on click ============================================== */
   $('.trigger').on('click', function() {
     $(this).parent().toggleClass('visible');
