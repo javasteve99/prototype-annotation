dc91e5b33695b14f636412817371bee8000c2fdc
Steve Hickey <steve@stevehickeydesign.com>
2013-10-11 11:06:02 -0400

    Cleanup some stuff

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@
   $('#annotations .annotation').each(function() {
     var annotationNumber = $(this).attr('id').split('-')[1];
     $('[data-annotation="' + annotationNumber + '"]').css('position', 'relative').append($('#annotation-' + annotationNumber));
+    $(this).prepend('<a href="#" class="trigger">' + annotationNumber + '</a>');
   });
 
   /* Ditch empty container for annotations ================================= */
